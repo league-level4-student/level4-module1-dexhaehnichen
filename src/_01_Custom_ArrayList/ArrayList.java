bf26f2b4d0fe3116c637724c10a2718f5f4f3cd1
League Student <league@iMac-12.attlocal.net>
2020-03-11 17:00:06 -0700

    CustomArrayList(IP)

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -6,8 +6,8 @@ public class ArrayList <T>{
 	
 	T[] array; 
 	
-	public <template> ArrayList(template o) {
-	array = (T[]) [0];
+	public ArrayList() {
+	array = (T[]) new Object[0];
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
@@ -15,24 +15,66 @@ public class ArrayList <T>{
 	}
 	
 	public void add(T val) {
-		T[] tempA = T[array.length+1];
+		T[] tempArray = (T[]) new Object[array.length+1];
+		tempArray[tempArray.length-1] = val;
 		
+		for (int i = 0; i < array.length; i++) {
+			tempArray[i] = array[i];
+		}
+		
+		array = tempArray;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
+		T[] tempArray = (T[]) new Object[array.length+1];
 		
+		for (int i = 0; i < tempArray.length; i++) {
+			if(i < loc) {
+				tempArray[i] = array[i];
+			}else if(i > loc) {
+				tempArray[i] = array[i-1];
+			}else {
+				tempArray[i] = val;
+			}
+		}
+		
+		array = tempArray;
 	}
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+		array[loc] = val;
 	}
 	
 	public void remove(int loc) throws IndexOutOfBoundsException {
+		T[] tempArray = (T[]) new Object[array.length-1];
 		
+		for (int i = 0; i < tempArray.length; i++) {
+			if(i < loc) {
+				tempArray[i] = array[i];
+			}else if(i > loc) {
+				tempArray[i-1] = array[i];
+			}
+		}
+		
+		array = tempArray;
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+		System.out.println("---");
 	}
 	
 	public boolean contains(T val) {
+
+		for (int i = 0; i < array.length; i++) {
+			if(array[i] == val) {
+				return true;
+			}
+		}
 		
 		return false;
+	}
+	
+	public int size() {
+		return array.length;
 	}
 }
